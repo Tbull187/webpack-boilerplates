c4bec054b2e3ed0bac709a206ddf09701f930d39
Tristan Bull <tristan.tbull@gmail.com>
2018-02-28 22:16:06 -0800

    react rendering

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import '../css/app.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-(function() {
-    console.log('hello world');
-}());
+ReactDOM.render(
+    <h1>hello world</h1>,
+    document.getElementById('content')
+);
+
+// (function() {
+//     console.log('hello world');
+// }());
